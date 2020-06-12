f96a3b8057901b579f847abbaaf8fd1411df1910
johnwr-response <52628290+johnwr-response@users.noreply.github.com>
2020-06-12 16:35:32 +0200

    Initial commit

---
+++ src/main/java/no/responseweb/imagearchive/filestoreservice/FileStoreServiceApplication.java
@@ -0,0 +1,13 @@
+package no.responseweb.imagearchive.filestoreservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileStoreServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileStoreServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/no/responseweb/imagearchive/filestoreservice/FileStoreServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package no.responseweb.imagearchive.filestoreservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileStoreServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
