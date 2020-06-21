3f1e35698cd5ed679fda7fff06b3146263eae33f
johnwr-response <52628290+johnwr-response@users.noreply.github.com>
2020-06-22 01:37:52 +0200

    Refactored all db related to dbutils module

--- src/main/java/no/responseweb/imagearchive/filestoreservice/FileStoreServiceApplication.java
+++ src/main/java/no/responseweb/imagearchive/filestoreservice/FileStoreServiceApplication.java
@@ -1,9 +1,12 @@
 package no.responseweb.imagearchive.filestoreservice;
 
+import no.responseweb.imagearchive.filestoredbservice.config.DBModuleConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(DBModuleConfig.class)
 public class FileStoreServiceApplication {
 
 	public static void main(String[] args) {
--- src/main/java/no/responseweb/imagearchive/filestoreservice/services/FileStoreListener.java
+++ src/main/java/no/responseweb/imagearchive/filestoreservice/services/FileStoreListener.java
@@ -2,6 +2,14 @@ package no.responseweb.imagearchive.filestoreservice.services;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import no.responseweb.imagearchive.filestoredbservice.domain.FilePath;
+import no.responseweb.imagearchive.filestoredbservice.domain.FileStore;
+import no.responseweb.imagearchive.filestoredbservice.mappers.FileItemMapper;
+import no.responseweb.imagearchive.filestoredbservice.mappers.FilePathMapper;
+import no.responseweb.imagearchive.filestoredbservice.mappers.FileStoreMapper;
+import no.responseweb.imagearchive.filestoredbservice.repositories.FileItemRepository;
+import no.responseweb.imagearchive.filestoredbservice.repositories.FilePathRepository;
+import no.responseweb.imagearchive.filestoredbservice.repositories.FileStoreRepository;
 import no.responseweb.imagearchive.filestoreservice.config.JmsConfig;
 import no.responseweb.imagearchive.model.FileItemDto;
 import no.responseweb.imagearchive.model.FilePathDto;
@@ -14,14 +22,16 @@ import org.springframework.stereotype.Component;
 @Slf4j
 @RequiredArgsConstructor
 @Component
-public class FileStoreRequestListener {
+public class FileStoreListener {
 
     private final JmsTemplate jmsTemplate;
     // TODO: Move all database config and executions into separate db module. Then enable these
-//    private final FileStoreRepository fileStoreRepository;
-//    private final FilePathRepository filePathRepository;
-//    private final FileItemRepository fileItemRepository;
-
+    private final FileStoreRepository fileStoreRepository;
+    private final FilePathRepository filePathRepository;
+    private final FileItemRepository fileItemRepository;
+    private final FileStoreMapper fileStoreMapper;
+    private final FilePathMapper filePathMapper;
+    private final FileItemMapper fileItemMapper;
 
     @JmsListener(destination = JmsConfig.FILE_STORE_REQUEST_QUEUE)
     public void listen(FileStoreRequestDto fileStoreRequest) {
@@ -29,7 +39,12 @@ public class FileStoreRequestListener {
         FilePathDto filePath = fileStoreRequest.getFilePath();
         FileItemDto fileItem = fileStoreRequest.getFileItem();
         if (filePath.getId() == null) {
-            log.info("Crete new sub-path: {}", filePath);
+            FileStore byNickname = fileStoreRepository.findByNickname(fileStore.getNickname());
+            log.info("Store: {}", fileStoreMapper.fileStoreToFileStoreDto(byNickname));
+            FilePath createdFilePath = filePathRepository.saveAndFlush(filePathMapper.filePathDtoToFilePath(filePath));
+            fileItem.setFileStorePathId(createdFilePath.getId());
+            filePath = filePathMapper.filePathToFilePathDto(createdFilePath);
+            log.info("Created new sub-path: {}", filePathMapper.filePathToFilePathDto(createdFilePath));
         }
 
         switch (fileStoreRequest.getFileStoreRequestType()) {
